51aaccceb81cf9bdb8d8ddfad068edac8b0a3950
putilovskaya <putilovskayaa@mail.ru>
2024-01-28 10:32:33 +0300

    add fixture to test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,17 +1,30 @@
+import pytest
+
 from src.item import Item
 
-item1 = Item('milk', 119.5, 10)
-item2 = Item('bread', 60.5, 20)
+
+@pytest.fixture
+def items():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    return item1, item2
 
 
-def test_calculate_total_price():
-    assert Item.calculate_total_price(item1) == 1195.0
-    assert Item.calculate_total_price(item2) == 1210.0
+def test_calculate_total_price(items):
+    assert Item.calculate_total_price(items[0]) == 200000
+    assert Item.calculate_total_price(items[1]) == 100000
 
 
-def test_apply_discount():
-    Item.pay_rate -= 0.1
-    item1.apply_discount()
-    item2.apply_discount()
-    assert item1.price == 107.55
-    assert item2.price == 54.45
+def test_apply_discount(items):
+    Item.pay_rate = 0.8
+    items[0].apply_discount()
+    assert items[0].price == 8000.0
+    assert items[1].price == 20000
+
+
+def test_name(items):
+    assert items[0].name == 'Смартфон'
+    items[0].name = 'Суперсмартфон'
+    assert items[0].name == 'Суперсмарт'
+    items[0].name = 'Смартфон'
+    assert items[0].name == 'Смартфон'
