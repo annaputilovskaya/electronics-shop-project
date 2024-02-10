30edafa2376671bafe5e2facbf0efeb68de66f4e
putilovskaya <putilovskayaa@mail.ru>
2024-02-10 20:52:44 +0300

    fix phone.py

--- src/phone.py
+++ src/phone.py
@@ -14,7 +14,7 @@ class Phone(Item):
         :param number_of_sim: Количество поддерживаемых сим-карт
         """
         super().__init__(name, price, quantity)
-        self.__number_of_sim = number_of_sim
+        self.number_of_sim = number_of_sim
 
     def __repr__(self):
         """
